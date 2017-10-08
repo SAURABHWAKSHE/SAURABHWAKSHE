5e922f0d9e5b1e610718c9d5c7d2a22cc8aa34cd
SAURABHWAKSHE <32311169+SAURABHWAKSHE@users.noreply.github.com>
2017-10-08 11:21:39 +0530

    Update SumOfTwoDice.java

--- SumOfTwoDice.java
+++ SumOfTwoDice.java
@@ -1,10 +1,28 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin com/bridgelabz/util/SumOfTwoDice.java
+ *  Execution:    java -cp bin com.bridgelabz.util.SumOfTwoDice
+ *  
+ *  Purpose:add two random no. obtained on two dice 
+ *
+ *  @author  Saurabh Wakshe
+ *  @version 1.8
+ *  @since   25-10-2017
+ *
+******************************************************************************/
+package com.bridgelabz.util;
+
 public class SumOfTwoDice { 
+	/*
+	*main method to generate random no. and add them 
+	*/
     public static void main(String[] args) {
         int SIDES = 6;
         int a = 1 + (int) (Math.random() * SIDES);
         int b = 1 + (int) (Math.random() * SIDES);
         int sum = a + b;
-       
+       	/*
+	*print the numbers and sum
+	*/
 	System.out.println(a);
 	System.out.println(b);
 	System.out.println(sum);
