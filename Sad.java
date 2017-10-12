a52d05e19acfba48f8f0d0e709e81add0b175aef
SAURABHWAKSHE <32311169+SAURABHWAKSHE@users.noreply.github.com>
2017-10-12 19:54:14 +0530

    Update Sad.java

--- Sad.java
+++ Sad.java
@@ -1,4 +1,16 @@
-public class Sad{
+/******************************************************************************
+  *  Compilation:  javac -d bin com/bridgelabz/util/RuntimeError.java
+  *  Execution:    java -cp bin com.bridgelabz.util.RuntimeError 
+  *  
+  *  Purpose:program to produce runtime error
+  *
+  *  @author  Saurabh Wakshe
+  *  @version 1.8
+  *  @since   27-09-2017
+  *
+ ******************************************************************************/
+package com.bridgelabz.util;
+public class RuntimeError{
 public void show (){
 System.out.println("hiii");
 }
