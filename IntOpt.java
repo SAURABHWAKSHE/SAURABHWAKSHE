3e15815d6efd45883350382ba82d5bcee1865e23
SAURABHWAKSHE <32311169+SAURABHWAKSHE@users.noreply.github.com>
2017-10-12 20:18:15 +0530

    Update IntOpt.java

--- IntOpt.java
+++ IntOpt.java
@@ -1,11 +1,34 @@
+/******************************************************************************
+  *  Compilation:  javac -d bin com/bridgelabz/util/IntOpt.java
+  *  Execution:    java -cp bin com.bridgelabz.util.IntOpt
+  *  
+  *  Purpose:program to perform diffrent operations on integer numbers
+  *
+  *  @author  Saurabh Wakshe
+  *  @version 1.8
+  *  @since   28-09-2017
+  *
+ ******************************************************************************/
+package com.bridgelabz.util;
 import java.util.Scanner;
 public class IntOpt{
 public static void main(String[]args){
+/*
+*Declaring variables
+*/
 int a,b,c;
+  
+/*
+*Accepting input through Scanner class
+*/
 Scanner sc=new Scanner(System.in);
 a=sc.nextInt();
 b=sc.nextInt();
 c=sc.nextInt();
+  
+/*
+*performing operations and printing the result
+*/
 System.out.println(a+b*c);
 System.out.println(a*b+c);
 System.out.println(c+a/b);
